8ebe44ea65f731b84e87c6dda8ff21310648d77a
dell <yc992147156@me.com>
2020-06-05 21:56:16 +0800

    Update

--- Makefile
+++ Makefile
@@ -1,28 +1,20 @@
 TARGET = LocalSSH
-VERSION = 0.1.0
-CC = xcrun -sdk iphoneos clang -arch arm64 -miphoneos-version-min=11.0
-LDID = ldid
+VERSION = 0.2.0
 
 .PHONY: all clean
 
 all: clean postinst prerm
-	mkdir com.michael.localssh-$(VERSION)_iphoneos-arm
-	mkdir com.michael.localssh-$(VERSION)_iphoneos-arm/DEBIAN
-	cp control com.michael.localssh-$(VERSION)_iphoneos-arm/DEBIAN
-	mv postinst com.michael.localssh-$(VERSION)_iphoneos-arm/DEBIAN
-	mv prerm com.michael.localssh-$(VERSION)_iphoneos-arm/DEBIAN
-	dpkg -b com.michael.localssh-$(VERSION)_iphoneos-arm
+	mkdir com.michael.localssh_$(VERSION)_iphoneos-arm
+	mkdir com.michael.localssh_$(VERSION)_iphoneos-arm/DEBIAN
+	cp control com.michael.localssh_$(VERSION)_iphoneos-arm/DEBIAN
+	mv postinst/.theos/obj/postinst prerm/.theos/obj/prerm com.michael.localssh_$(VERSION)_iphoneos-arm/DEBIAN
+	dpkg -b com.michael.localssh_$(VERSION)_iphoneos-arm
 
 postinst: clean
-	$(CC) postinst.c -o postinst
-	strip postinst
-	$(LDID) -Sentitlements.xml postinst
+	sh make-postinst.sh
 
 prerm: clean
-	$(CC) prerm.c -o prerm
-	strip prerm
-	$(LDID) -Sentitlements.xml prerm
+	sh make-prerm.sh
 
 clean:
-	rm -rf com.michael.localssh-*
-	rm -f postinst prerm
+	rm -rf com.michael.localssh_* postinst/.theos prerm/.theos
---
+++ prerm/prerm.m
@@ -0,0 +1,55 @@
+#include <CoreFoundation/CoreFoundation.h>
+
+bool modifyPlist(NSString *filename, void (^function)(id)) {
+    NSData *data = [NSData dataWithContentsOfFile:filename];
+    if (data == nil) {
+        return false;
+    }
+    NSPropertyListFormat format = 0;
+    NSError *error = nil;
+    id plist = [NSPropertyListSerialization propertyListWithData:data options:NSPropertyListMutableContainersAndLeaves format:&format error:&error];
+    if (plist == nil) {
+        return false;
+    }
+    if (function) {
+        function(plist);
+    }
+    NSData *newData = [NSPropertyListSerialization dataWithPropertyList:plist format:format options:0 error:&error];
+    if (newData == nil) {
+        return false;
+    }
+    if (![data isEqual:newData]) {
+        if (![newData writeToFile:filename atomically:YES]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void run_system(const char *cmd) {
+    int status = system(cmd);
+    if (WEXITSTATUS(status) != 0) {
+        printf("Error in command: \"%s\"\n", cmd);
+        exit(WEXITSTATUS(status));
+    }
+}
+
+int main() {
+    if (getuid() != 0) {
+        printf("Run this as root!\n");
+    }
+
+    run_system("launchctl unload /Library/LaunchDaemons/com.openssh.sshd.plist");
+    if (access("/usr/libexec/sshd-keygen-wrapper", F_OK) == 0) {
+        NSString *const plist = @"/Library/LaunchDaemons/com.openssh.sshd.plist";
+        modifyPlist(plist, ^(id plist) {
+            plist[@"Sockets"][@"LocalSSHListener"] = nil;
+        });
+    } else {
+        run_system("sed -i '/Port 2222/d' /etc/ssh/sshd_config");
+        run_system("sed -i 's/Port 22/#Port 22/' /etc/ssh/sshd_config");
+    }
+    run_system("launchctl load /Library/LaunchDaemons/com.openssh.sshd.plist");
+    return 0;
+}
+
